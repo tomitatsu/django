639ea4469cb6ff7ad7ba99bc0375717e35d362f0
Tatsuhiko.Tomita <tomita.tatsuhiko@gmail.com>
2019-03-10 07:36:13 -0700

    Add form

---
+++ logAnalyze/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class hornetUrlForm(forms.Form):
+     hornetUrl = forms.CharField()
--- logAnalyze/views.py
+++ logAnalyze/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from logAnalyze.forms import hornetUrlForm
 
-# Create your views here.
+def index(request):
+#    return HttpResponse("Hello, world. You're at the polls index.")
+#     return render(request, 'logAnalyze/index.html')
+	f =  hornetUrlForm()
+	return render(request, 'logAnalyze/index.html', {'form1': f})
+
+def results(request, url):
+#    response = "You're looking at the results of %s."
+#    return HttpResponse(response % url)
+     return render(request, 'logAnalyze/result.html', {'url': url})
+
