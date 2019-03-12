494ac9e568e354c27e4abbcf09dd144f01d8b5fe
Tatsuhiko.Tomita <tomita.tatsuhiko@gmail.com>
2019-03-12 14:07:49 -0700

    update form

--- logAnalyze/views.py
+++ logAnalyze/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from logAnalyze.forms import hornetUrlForm
 
 def index(request):
 #    return HttpResponse("Hello, world. You're at the polls index.")
 #     return render(request, 'logAnalyze/index.html')
-	f =  hornetUrlForm()
-	return render(request, 'logAnalyze/index.html', {'form1': f})
+	if request.method == 'POST':
+		c = {
+			'url':request.POST["hornetUrl"]
+		}
+	else:
+		f =  hornetUrlForm()
+		c = {'hornetForm': f}
+	return render_to_response('logAnalyze/index.html', c)
+		#return redirect('logAnalyze:result', {'hornetform': f})
+		#return render(request, 'logAnalyze/index.html', {'hornetform': f})
 
 def results(request, url):
 #    response = "You're looking at the results of %s."
