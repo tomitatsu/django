cd06936554d0c44ca9c1dfd4cb5b39c6e888b972
tomitatsu <tomita.tatsuhiko@gmail.com>
2019-04-05 18:00:56 +0900

    Show log models with bootstrap table

--- logAnalyze/views.py
+++ logAnalyze/views.py
@@ -1,27 +1,27 @@
 from django.shortcuts import render, redirect
 from logAnalyze.forms import hornetUrlForm
+from logAnalyze.models import Log
 
 def index(request):
-#    return HttpResponse("Hello, world. You're at the polls index.")
-#     return render(request, 'logAnalyze/index.html')
 	if request.method == 'POST':
-		c = {
-			'url':request.POST["hornetUrl"]
-		}
 		hornetUrl=request.POST["hornetUrl"]
 		return redirect('logAnalyze:analyze', url=hornetUrl)
 	else:
 		f =  hornetUrlForm()
 		c = {'hornetForm': f}
+	logs = Log.objects.all().order_by('-id')
+	c['logs'] = logs
 	return render(request, 'logAnalyze/index.html', c)
-		#return render(request, 'logAnalyze/index.html', {'hornetform': f})
+
 
 def analyze(request, url):
-	#return redirect('logAnalyze:results', {'url': url})
-	return render(request, 'logAnalyze/index.html', {'url': url})
+	print("URL=%s" % url)
+	log = Log(url=url, status=1)
+	log.save()
+	return redirect('logAnalyze:index')
+#	return render(request, 'logAnalyze/index.html', {'url': url})
+
 
 def results(request, url):
-#    response = "You're looking at the results of %s."
-#    return HttpResponse(response % url)
      return render(request, 'logAnalyze/result.html', {'url': url})
 
